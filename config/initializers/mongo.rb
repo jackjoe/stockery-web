f5f468c298fc7d34090ec9182a26e84eeaf30616
Pieter Michels <pieter@noort.be>
2011-10-05 09:00:11 +0200

    env issues

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,11 +1,9 @@
-p Rails.env
-p ENV
-
 if Rails.env.production?
   MongoMapper.config = {Rails.env => {:uri => ENV['MONGOHQ_URL']}}
   MongoMapper.connect(Rails.env)
 else
   db_config = YAML::load(File.read("#{Rails.root}/config/database.yml"))[Rails.env]
+  p db_config
 
   MongoMapper.connection = Mongo::Connection.new(db_config['host'], db_config['port'])
   MongoMapper.connection.add_auth(db_config['database'], db_config['user'], db_config['password'])
