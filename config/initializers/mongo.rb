ffaf20d5585e4da5c2e5b263dfe6a9e8f4d1346a
Pieter Michels <pieter@noort.be>
2011-10-05 08:52:28 +0200

    conn

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,14 +1,13 @@
-unless Rails.env == 'production'
+if Rails.env == 'production'
+  MongoMapper.config = {Rails.env => {:uri => ENV['MONGOHQ_URL']}}
+  MongoMapper.connect(Rails.env)
+else
   db_config = YAML::load(File.read("#{Rails.root}/config/database.yml"))[Rails.env]
 
-  conn = Mongo::Connection.new(db_config['host'], db_config['port'])
-  conn.add_auth(db_config['database'], db_config['user'], db_config['password'])
+  MongoMapper.connection = Mongo::Connection.new(db_config['host'], db_config['port'])
+  MongoMapper.connection.add_auth(db_config['database'], db_config['user'], db_config['password'])
 
-  MongoMapper.connection = conn
   MongoMapper.database = db_config['database']
-else
-  MongoMapper.config = {:production => {:uri => ENV['MONGOHQ_URL']}}
-  MongoMapper.connect(:production)
 end
 
 if defined?(PhusionPassenger)
