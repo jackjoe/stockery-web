0d44413134a5aaefd993b50585c2306c86034074
Pieter Michels <pieter@noort.be>
2011-10-05 08:49:28 +0200

    mongohq env url for heroku

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,10 +1,15 @@
-db_config = YAML::load(File.read("#{Rails.root}/config/database.yml"))[Rails.env]
+unless Rails.env == 'production'
+  db_config = YAML::load(File.read("#{Rails.root}/config/database.yml"))[Rails.env]
 
-conn = Mongo::Connection.new(db_config['host'], db_config['port'])
-conn.add_auth(db_config['database'], db_config['user'], db_config['password'])
+  conn = Mongo::Connection.new(db_config['host'], db_config['port'])
+  conn.add_auth(db_config['database'], db_config['user'], db_config['password'])
 
-MongoMapper.connection = conn
-MongoMapper.database = db_config['database']
+  MongoMapper.connection = conn
+  MongoMapper.database = db_config['database']
+else
+  MongoMapper.config = {:production => {:uri => ENV['MONGOHQ_URL']}}
+  MongoMapper.connect(:production)
+end
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|
