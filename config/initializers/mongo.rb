781e62ffda937b96d299ad3fe2d88980d0ff3dd1
Pieter Michels <pieter@noort.be>
2011-10-05 09:33:11 +0200

    alerting

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,11 +1,18 @@
+p Rails
+p ::Rails.env
+p Rails.env.production?
+p ::Rails.env.production?
+
 if Rails.env.production?
   MongoMapper.config = {Rails.env => {:uri => ENV['MONGOHQ_URL']}}
   MongoMapper.connect(Rails.env)
 
+  puts "mongomapper"
   p MongoMapper
 else
   db_config = YAML::load(File.read("#{Rails.root}/config/database.yml"))[::Rails.env]
-
+  
+  puts "db-config"
   p db_config
 
   MongoMapper.connection = Mongo::Connection.new(db_config['host'], db_config['port'])
