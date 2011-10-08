fc3fdc9fcfdc366226da6d622d8407d521154947
Pieter Michels <pieter@noort.be>
2011-10-08 08:47:34 +0200

    db changes

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,14 +1,13 @@
-
 if Rails.env.production? && ENV['MONGOHQ_URL'] # heroku
   puts "Production mode with #{ENV['MONGOHQ_URL']}"
 
-  # MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com', 10093)
-  # MongoMapper.connection.add_auth('app1214616', 'heroku', '0d5b490776402b4ca42a79174e9b5e66')
+  MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com', 10093)
+  MongoMapper.connection.add_auth('app1214616', 'heroku', '0d5b490776402b4ca42a79174e9b5e66')
 
-  # MongoMapper.database = 'app1214616'
+  MongoMapper.database = 'app1214616'
   
-  MongoMapper.config = {Rails.env => {:uri => ENV['MONGOHQ_URL']}}
-  MongoMapper.connect(Rails.env)
+  # MongoMapper.config = {Rails.env => {:uri => ENV['MONGOHQ_URL']}}
+  # MongoMapper.connect(Rails.env)
 else
   db_config = YAML::load(File.read("#{Rails.root}/config/database.yml"))[::Rails.env]
 
