b14d889565a9006d8cb0f152c385e35d57f9738d
Pieter Michels <pieter@noort.be>
2011-10-08 08:31:02 +0200

    removing submodule because of heroku

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,19 +1,19 @@
-  MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com', 10093)
-  MongoMapper.connection.add_auth('app1214616', 'heroku', '0d5b490776402b4ca42a79174e9b5e66')
 
-  MongoMapper.database = 'app1214616'
+if Rails.env.production? && ENV['MONGOHQ_URL'] # heroku
+  puts "Production mode with #{ENV['MONGOHQ_URL']}"
 
-# if Rails.env.production? && ENV['MONGOHQ_URL'] # heroku
-#   puts "Production mode with #{ENV['MONGOHQ_URL']}"
-# 
-#   MongoMapper.config = {Rails.env => {:uri => ENV['MONGOHQ_URL']}}
-#   p MongoMapper.config
-#   MongoMapper.connect(Rails.env)
-# else
-#   db_config = YAML::load(File.read("#{Rails.root}/config/database.yml"))[::Rails.env]
-# 
-#   MongoMapper.connection = Mongo::Connection.new(db_config['host'], db_config['port'])
-#   MongoMapper.connection.add_auth(db_config['database'], db_config['user'], db_config['password'])
-# 
-#   MongoMapper.database = db_config['database']
-# end
+  # MongoMapper.connection = Mongo::Connection.new('staff.mongohq.com', 10093)
+  # MongoMapper.connection.add_auth('app1214616', 'heroku', '0d5b490776402b4ca42a79174e9b5e66')
+
+  # MongoMapper.database = 'app1214616'
+  
+  MongoMapper.config = {Rails.env => {:uri => ENV['MONGOHQ_URL']}}
+  MongoMapper.connect(Rails.env)
+else
+  db_config = YAML::load(File.read("#{Rails.root}/config/database.yml"))[::Rails.env]
+
+  MongoMapper.connection = Mongo::Connection.new(db_config['host'], db_config['port'])
+  MongoMapper.connection.add_auth(db_config['database'], db_config['user'], db_config['password'])
+
+  MongoMapper.database = db_config['database']
+end
